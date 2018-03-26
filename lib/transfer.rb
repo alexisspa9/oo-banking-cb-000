16ea529a39ace27e8d49ba76149f1e435992ef48
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 15:12:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,8 +11,12 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
   def execute_transaction
+    if @sender.valid?
     @receiver.balance += @amount
     @sender.balance -= @amount
     @status = 'complete'
+  else
+    "Transaction rejected. Please check your account balance."
+  end
   end
 end
